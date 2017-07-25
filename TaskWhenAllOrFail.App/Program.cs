89c76ffa2115d91b231d62d9d154a9d912e3a3b3
ahron levi <ahron.levi@zerto.com>
2017-07-25 10:26:42 +0300

    Task when all or fail

---
+++ TaskWhenAllOrFail.App/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskWhenAllOrFail.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Func<CancellationToken, int>> funcs = new List<Func<CancellationToken, int>>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int index = i;
+
+                if (i == 500)
+                {
+                    funcs.Add((cToken) => { throw new NotImplementedException(); });
+                }
+                else
+                {
+                    funcs.Add((cToken) =>
+                    {
+                        if (cToken.IsCancellationRequested)
+                        {
+                            Console.WriteLine($"I was canceled id:{index}");
+                            return -1;
+                        }
+
+                        Console.WriteLine($"Task number {index} is sleeping");
+
+                        var rand = new Random(100);
+
+                        Thread.Sleep(rand.Next(100, 200));
+
+                        if (cToken.IsCancellationRequested)
+                        {
+                            Console.WriteLine($"I was canceled id:{index}");
+                            return -1;
+                        }
+
+                        Console.WriteLine($"Task number {index} is done");
+                        return index;
+                    });
+                }
+
+            }
+
+            Task<int[]> task = funcs.WhenAllOrFail();
+
+            try
+            {
+                task.Wait();
+
+                int[] result = task.Result;
+
+
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine($"result number {i} was {result[i]}");
+                }
+
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.Flatten().InnerExceptions)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ TaskWhenAllOrFail.App/TaskEx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskWhenAllOrFail.App
+{
+    static class TaskEx
+    {
+        public static Task WhenAllOrFail(this IEnumerable<Action<CancellationToken>> actions)
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            List<Task> allTasks = actions.Select(x =>
+            {
+                var token = source.Token;
+
+                Action aAction = () =>
+                {
+                    if (token.IsCancellationRequested) return;
+
+                    try
+                    {
+                        x(token);
+                    }
+                    catch (Exception)
+                    {
+                        source.Cancel();
+                        throw;
+                    }
+                };
+
+                return Task.Factory.StartNew(aAction, source.Token, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+
+            }).ToList();
+
+            if (allTasks.Count == 0)
+                throw new ArgumentException("No tasks to wait on");
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            int tasksCompletedCount = 0;
+
+            Action<Task> completedAction = t =>
+            {
+                if (t.IsFaulted)
+                {
+                    tcs.TrySetException(t.Exception);
+                    tcs.SetResult(false);
+                    return;
+                }
+                if (t.IsCanceled)
+                {
+                    tcs.TrySetCanceled();
+                    tcs.SetResult(false);
+                    return;
+                }
+                if (Interlocked.Increment(ref tasksCompletedCount) == allTasks.Count)
+                {
+                    tcs.SetResult(true);
+                }
+            };
+
+            allTasks.ForEach(t => t.ContinueWith(completedAction, TaskContinuationOptions.ExecuteSynchronously));
+
+            return tcs.Task;
+        }
+
+        public static Task<T[]> WhenAllOrFail<T>(this IEnumerable<Func<CancellationToken, T>> funcs)
+        {
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            List<Task<T>> allTasks = funcs.Select(x =>
+            {
+                var token = source.Token;
+
+                Func<T> aFunc = () =>
+                {
+                    if (token.IsCancellationRequested) return default(T);
+
+                    try
+                    {
+                        return x(token);
+                    }
+                    catch (Exception)
+                    {
+                        source.Cancel();
+                        throw;
+                    }
+                };
+
+                return Task<T>.Factory.StartNew(aFunc, source.Token, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+
+            }).ToList();
+
+            if (allTasks.Count == 0)
+                throw new ArgumentException("No tasks to wait on");
+
+            var tcs = new TaskCompletionSource<T[]>();
+
+            int tasksCompletedCount = 0;
+
+            Action<Task<T>> completedAction = t =>
+            {
+                if (t.IsFaulted)
+                {
+                    tcs.TrySetException(t.Exception);
+                    return;
+                }
+                if (t.IsCanceled)
+                {
+                    tcs.TrySetCanceled();
+                    return;
+                }
+                if (Interlocked.Increment(ref tasksCompletedCount) == allTasks.Count)
+                {
+                    tcs.SetResult(allTasks.Select(ct => ct.Result).ToArray());
+                }
+            };
+
+            allTasks.ForEach(t => t.ContinueWith(completedAction, TaskContinuationOptions.ExecuteSynchronously));
+
+            return tcs.Task;
+        }
+    }
+}
