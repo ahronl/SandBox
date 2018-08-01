c6cb0332d7d658c5b8d1724a3240089cbb50aaa7
ahronl <aharon.levi@amobee.com>
2018-08-01 11:03:55 +0300

    move to dotnet core

--- MostResentlyUsedRepository.App/Program.cs
+++ MostResentlyUsedRepository.App/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MostResentlyUsedRepository.App
 {
@@ -10,24 +6,7 @@ namespace MostResentlyUsedRepository.App
     {
         static void Main(string[] args)
         {
-            var repository = new MostResentlyUsedRepository<int, int>(3);
-
-            repository.Put(1, 1);
-            repository.Put(2, 2);
-            repository.Get(2);
-            repository.Get(1);
-            repository.Put(3, 2);
-            repository.Put(3, 1);
-            repository.Put(3, 5);
-            repository.Put(3, 6);
-            repository.Put(2, 2);
-
-            foreach (var item in repository.All())
-            {
-                Console.WriteLine("key {0} value {1}", item.Item1, item.Item2);
-            }
-
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
