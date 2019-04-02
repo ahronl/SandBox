db86cc089a6f09d404ff0ffa6bb2252798b66f27
ahron levi <aharon.levi@amobee.com>
2019-04-02 16:23:58 +0300

    Update ZTimerLogger.cs

--- TimerWrraper/Log/ZTimerLogger.cs
+++ TimerWrraper/Log/ZTimerLogger.cs
@@ -3,11 +3,11 @@ using NLog;
 
 namespace TimerWrapper.Log
 {
-    internal class ZTimerLogger : ZTimerLoggerBase
+    internal class TimerLogger : TimerLoggerBase
     {
         private static readonly Logger s_logger = LogManager.GetCurrentClassLogger();
 
-        public ZTimerLogger(ITimerEvents timer)
+        public TimerLogger(ITimerEvents timer)
             : base(timer)
         {
         }
@@ -22,22 +22,22 @@ namespace TimerWrapper.Log
 
         protected override void LogExceptionEvent(ITimer sender, Exception ex)
         {
-            s_logger.Error(ex, $"ZTimer encounter an error Timer Id : {sender.Id} Timer name: {sender.Name} Timer GUID: {sender.InstanceId}");
+            s_logger.Error(ex, $"Timer encounter an error Timer Id : {sender.Id} Timer name: {sender.Name} Timer GUID: {sender.InstanceId}");
         }
 
         protected override void LogCanceledEvent(ITimer sender)
         {
-            s_logger.Info($"ZTimer canceled execution Timer Id :{ sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
+            s_logger.Info($"Timer canceled execution Timer Id :{ sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
         }
 
         protected override void LogStopEvent(ITimer sender)
         {
-            s_logger.Info($"ZTimer stopped Timer Id :{sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
+            s_logger.Info($"Timer stopped Timer Id :{sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
         }
 
         protected override void LogStartEvent(ITimer sender)
         {
-            s_logger.Info($"ZTimer started Timer Id :{sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
+            s_logger.Info($"Timer started Timer Id :{sender.Id}, Timer name : {sender.Name} Timer GUID: {sender.InstanceId}");
         }
     }
 }
