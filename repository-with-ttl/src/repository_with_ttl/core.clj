25210022b9b1822385f32011695c0b661bdad08d
ahronl <aharon.levi@amobee.com>
2024-10-28 08:01:29 +0200

    use only one state map

--- repository-with-ttl/src/repository_with_ttl/core.clj
+++ repository-with-ttl/src/repository_with_ttl/core.clj
@@ -2,36 +2,33 @@
   (:require [clj-time.core :as t]
             [clojure.core.async :as a]))
 
-(def ^:private my-time-state (atom {}))
 (def ^:private my-state (atom {}))
 
 (defn- periodic-clean [n]
-   (let [ks (->> @my-time-state
-                 (filter (fn [[k tm]] (t/before? tm (t/minus (t/now) (t/minutes n)))))
-                 (keys)
-                 (into #{}))
-         rm (fn [s] (->> s
-                          (remove (fn [[k v]] 
-                                    (contains? ks k)))
-                          (into {})))]
-     (swap! my-time-state rm)
-     (swap! my-state rm)))
+   (let [ks (->> 
+              @my-state
+              (filter (fn [[k v]] 
+                        (t/before? (:time v) (t/minus (t/now) (t/minutes n)))))
+              (keys)
+              (into #{}))]
+     (swap! my-state (fn [s] (->> 
+                               s
+                               (remove (fn [[k _]] (contains? ks k)))
+                               (into {}))))))
 
-(defn- run-periodicly [f];;;make it better
+(defn- run-periodicly [f]
   (a/go-loop []
-             (prn f)
-             (prn "running .. ")
              (f)
              (Thread/sleep 1000)
-             (prn "done")
              (recur)))
 
 (defn init [n]
   (run-periodicly (partial periodic-clean n)))
 
 (defn put-in-state [k v]
-  (swap! my-state assoc k v)
-  (swap! my-time-state assoc k (t/now)))
+  (swap! my-state assoc k {:val v :time (t/now)}))
 
 (defn get-from-state [k]
-  (get @my-state k))
+  (-> @my-state
+      (get k)
+      (get :val)))
